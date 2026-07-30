c0f7f3da3e734a66937ed72046e3ad81ca7dfe6c
salarghanbari1388-ux <salarghanbari1388@gmail.com>
2026-07-30 16:13:59 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import os
-import time
+import database
+from game import register_user, get_user
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام! ربات سالار آماده است 🤖")
+    user = update.effective_user
+
+    register_user(user.id, user.username or user.first_name)
+
+    player = get_user(user.id)
+
+    await update.message.reply_text(
+        f"""🎮 سلام {user.first_name}
+
+👤 پروفایل شما ساخته شد.
+
+🪙 سکه: {player[2]}
+⭐ سطح: {player[3]}
+
+به دنیای شکار گنج خوش اومدی!"""
+    )
 
 app = Application.builder().token(TOKEN).build()
 
