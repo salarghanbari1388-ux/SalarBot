f13a872048376b8912786ddf1819dab225de7e16
salarghanbari1388-ux <salarghanbari1388@gmail.com>
2026-07-30 16:34:55 +0330

    Update main.py

--- main.py
+++ main.py
@@ -63,5 +63,5 @@ app = Application.builder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, button_handler))
-
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, check_riddle_answer))
 app.run_polling()
