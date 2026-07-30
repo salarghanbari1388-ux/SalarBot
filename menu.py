39a5edbdce8fc4418e34f04d9ccf131b1b8685dd
salarghanbari1388-ux <salarghanbari1388@gmail.com>
2026-07-30 17:34:12 +0330

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,10 +1,11 @@
 from telegram import ReplyKeyboardMarkup
 
+
 def main_menu():
     keyboard = [
-        ["🎮 شروع بازی", "👤 پروفایل"],
-        ["🏆 رتبه‌بندی", "⭐ VIP"],
-        ["🎁 دعوت دوستان"]
+        ["🎮 شروع بازی"],
+        ["👤 پروفایل", "🏆 رتبه‌بندی"],
+        ["⭐ VIP", "🎁 دعوت دوستان"]
     ]
 
     return ReplyKeyboardMarkup(
