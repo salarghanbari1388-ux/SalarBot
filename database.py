3c35e90b38865c92a3efc055d51925f7f38f2ca0
salarghanbari1388-ux <salarghanbari1388@gmail.com>
2026-07-30 18:34:35 +0330

    Update database.py

--- database.py
+++ database.py
@@ -6,6 +6,7 @@ def add_user(user_id, username):
         users[user_id] = {
             "username": username,
             "score": 0,
+            "games": 0,
             "vip": False
         }
 
@@ -17,3 +18,11 @@ def get_user(user_id):
 def add_score(user_id, points):
     if user_id in users:
         users[user_id]["score"] += points
+        users[user_id]["games"] += 1
+
+
+def get_score(user_id):
+    if user_id in users:
+        return users[user_id]["score"]
+
+    return 0
