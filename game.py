4d3722aac67c7d5c337e397abd7ad52e961b851f
salarghanbari1388-ux <salarghanbari1388@gmail.com>
2026-07-30 17:35:19 +0330

    Update game.py

--- game.py
+++ game.py
@@ -1,29 +1,5 @@
-import sqlite3
+from database import add_user
+
 
 def register_user(user_id, username):
-    conn = sqlite3.connect("data.db")
-    cursor = conn.cursor()
-
-    cursor.execute(
-        "INSERT OR IGNORE INTO users(user_id, username) VALUES(?, ?)",
-        (user_id, username)
-    )
-
-    conn.commit()
-    conn.close()
-
-
-def get_user(user_id):
-    conn = sqlite3.connect("data.db")
-    cursor = conn.cursor()
-
-    cursor.execute(
-        "SELECT * FROM users WHERE user_id=?",
-        (user_id,)
-    )
-
-    user = cursor.fetchone()
-
-    conn.close()
-
-    return user
+    add_user(user_id, username)
