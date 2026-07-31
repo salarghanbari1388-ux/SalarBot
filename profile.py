b52cd60b6ef8d9e33a86ee7281d649e2da3f42bc
salarghanbari1388-ux <salarghanbari1388@gmail.com>
2026-07-31 16:11:58 +0330

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,14 +1,13 @@
 from database import get_user
 
-
 def profile_text(user_id):
     user = get_user(user_id)
-
     if user is None:
         return "❌ هنوز پروفایلی ساخته نشده."
-
-    vip_status = "⭐ فعال" if user["vip"] else "❌ ندارد"
-
+    
+    from vip_manager import is_vip
+    vip_status = "⭐ فعال" if is_vip(user_id) else "❌ ندارد"
+    
     return (
         "👤 پروفایل بازیکن\n\n"
         f"🆔 نام: {user['username']}\n"
