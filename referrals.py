c5a88a82a979315238cc5c1d4b98a6fa43650920
salarghanbari1388-ux <salarghanbari1388@gmail.com>
2026-07-31 16:14:23 +0330

    Update referrals.py

--- referrals.py
+++ referrals.py
@@ -1,57 +1,19 @@
-# referrals.py
-
+from config import BOT_USERNAME
 
 referrals = {}
-
 invited_by = {}
 
-
-
 def add_referral(user_id, inviter_id):
-
-    if user_id == inviter_id:
+    if user_id == inviter_id or user_id in invited_by:
         return
-
-
-    if user_id in invited_by:
-        return
-
-
     invited_by[user_id] = inviter_id
-
-
-    if inviter_id not in referrals:
-
-        referrals[inviter_id] = 0
-
-
-    referrals[inviter_id] += 1
-
-
-
+    referrals[inviter_id] = referrals.get(inviter_id, 0) + 1
 
 def get_referrals(user_id):
-
-    return referrals.get(
-        user_id,
-        0
-    )
-
-
+    return referrals.get(user_id, 0)
 
 def referral_link(user_id):
-
-    return (
-        f"https://t.me/YOUR_BOT_USERNAME"
-        f"?start={user_id}"
-    )
-
-
+    return f"https://t.me/{BOT_USERNAME}?start={user_id}"
 
 def top_referrers():
-
-    return sorted(
-        referrals.items(),
-        key=lambda x:x[1],
-        reverse=True
-  )
+    return sorted(referrals.items(), key=lambda x: x[1], reverse=True)
