86d42c165bbe819979031889d98679471eb8ac0d
salarghanbari1388-ux <salarghanbari1388@gmail.com>
2026-07-31 16:13:07 +0330

    Update ranking.py

--- ranking.py
+++ ranking.py
@@ -1,22 +1,11 @@
 from database import users
 
-
 def ranking_text():
     if not users:
         return "🏆 هنوز بازیکنی وجود ندارد."
-
-    players = sorted(
-        users.values(),
-        key=lambda x: x["score"],
-        reverse=True
-    )
-
+    
+    players = sorted(users.values(), key=lambda x: x["score"], reverse=True)
     text = "🏆 رتبه‌بندی بازیکنان:\n\n"
-
     for i, player in enumerate(players[:10], start=1):
-        text += (
-            f"{i}️⃣ {player['username']} "
-            f"➜ {player['score']} امتیاز\n"
-        )
-
+        text += f"{i}️⃣ {player['username']} ➜ {player['score']} امتیاز\n"
     return text
